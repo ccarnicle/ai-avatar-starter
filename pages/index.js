4142ace529e9b887b6a8cc0fbdd3476a9303034e
CJ Carnicle <cj@aisportspro.com>
2023-01-12 21:37:31 -0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,65 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState(''); 
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = async () => {
+    console.log('Generating...');
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+    
+    const data = await response.json();
+  
+    if (response.status === 503) {
+      console.log('Model still loading...');
+      return;
+    }
+  
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+  
+    // Set image data into state property
+    setImg(data.image);
+  }
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>CJs AI Avatar Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Paint me like one of your french girls</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "cjcarnicle" in the prompt</h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange}/>
+            <div className="prompt-buttons">
+              <a className="generate-button">
+                <div className="generate" onClick={generateAction}>
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
